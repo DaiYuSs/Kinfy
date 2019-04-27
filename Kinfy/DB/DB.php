b233e36507782df2e9930350eddfe832f318168c
DYaiu <daiyuss@126.com>
2019-04-27 21:41:15 +0800

    DB ----------- 新增join 新增一系列where方法 重写where 重写get

--- Kinfy/DB/DB.php
+++ Kinfy/DB/DB.php
@@ -10,7 +10,15 @@ class DB
     private $where = [];
     private $table = '';
     private $limit = [];
+    private $fields = '*';
+    public $sql = [];
+    public $join = '';
 
+    /**
+     * DB初始化连接数据库和全局属性
+     *
+     * @param null $conf
+     */
     public function __construct($conf = null)
     {
         if ($conf == null) {
@@ -55,7 +63,6 @@ class DB
         return $this;
     }
 
-
     public function take($num)
     {
         return $this->limit(0, $num);
@@ -85,9 +92,190 @@ class DB
      * @param string $val
      * @return $this
      */
-    public function where($field, $op, $val)
+    public function where($field, $op, $val = null)
     {
-        $this->where[] = [$field, $op, $val];
+        return $this->andWhere($field, $op, $val);
+    }
+
+    /**
+     * where 条件语句存储
+     *
+     * @param string $field
+     * @param string $op
+     * @param string $val
+     * @param string $type
+     * @return $this
+     */
+    private function _where($field, $op, $val, $type)
+    {
+        if ($val === null) {
+            $val = $op;
+            $op = '=';
+        }
+        $this->where[] = [$field, $op, $val, $type];
+        return $this;
+    }
+
+    /**
+     *
+     * @param $field
+     * @param $op
+     * @param null $val
+     * @return DB
+     */
+    public function andWhere($field, $op, $val = null)
+    {
+        return $this->_where($field, $op, $val, 'and');
+    }
+
+    /**
+     *
+     * @param $field
+     * @param $op
+     * @param null $val
+     * @return DB
+     */
+    public function orWhere($field, $op, $val = null)
+    {
+        return $this->_where($field, $op, $val, 'or');
+    }
+
+    /**
+     * 指向andWhereNull
+     *
+     * @param $field
+     * @return $this
+     */
+    public function whereNull($field)
+    {
+        return $this->andWhereNull($field);
+    }
+
+    /**
+     *
+     * @param $field
+     * @return $this
+     */
+    public function andWhereNull($field)
+    {
+        return $this->_where($field, 'IS', 'NULL', 'and');
+    }
+
+    /**
+     *
+     * @param $field
+     * @return $this
+     */
+    public function orWhereNull($field)
+    {
+        return $this->_where($field, 'IS', 'NULL', 'or');
+    }
+
+    /**
+     * 指向andWhereNotNull
+     *
+     * @param $field
+     * @return $this
+     */
+    public function whereNotNull($field)
+    {
+        return $this->andWhereNotNull($field);
+    }
+
+    /**
+     *
+     * @param $field
+     * @return $this
+     */
+    public function andWhereNotNull($field)
+    {
+        return $this->_where($field, 'IS NOT', 'NULL', 'and');
+    }
+
+    /**
+     *
+     * @param string $field
+     * @return $this
+     */
+    public function orWhereNotNull($field)
+    {
+        return $this->_where($field, 'IS NOT', 'NULL', 'or');
+    }
+
+    /**
+     * 插入一个'('
+     *
+     * @return $this
+     */
+    public function L()
+    {
+        return $this->_where('(', '', '', '');
+    }
+
+    /**
+     * 插入一个')'
+     *
+     * @return $this
+     */
+    public function R()
+    {
+        return $this->_where(')', '', '', '');
+    }
+
+    /**
+     * 根据指定条件生成SQL语句与预处理的值
+     *
+     * @return array
+     */
+    public function genSql()
+    {
+        if ($this->sql != []) {
+            return $this->sql;
+        }
+        list($WHERE, $VAL) = $this->genWhere();
+        $limit = '';
+        if ($this->limit) {
+            $limit = " limit {$this->limit[0]},{$this->limit[1]} ";
+        }
+        //1.准备SQL
+        $this->sql['sql'] = "select {$this->fields} from {$this->table} {$this->join} $WHERE {$limit}";
+        $this->sql['value'] = $VAL;
+        return $this->sql;
+    }
+
+    /**
+     * 简易的join暴力插入
+     *
+     * @param string $table
+     * @param string $condition
+     * @return $this
+     */
+    public function join($table, $condition)
+    {
+        $this->join = "JOIN {$table} ON {$condition}";
+        return $this;
+    }
+
+    /**
+     * 接收任意个参数作为sql语句指定列
+     *
+     * @param mixed ...$fields
+     * @return $this
+     */
+    public function select(...$fields)
+    {
+        if (!empty($fields)) {
+            $sep = '';
+            $this->fields = '';
+            foreach ($fields as $f) {
+                $is_identifier = strpos($f, '.') !== false;
+                if (!$is_identifier) {
+                    $f = "`{$f}`";
+                }
+                $this->fields .= "{$sep}{$f}";
+                $sep = ',';
+            }
+        }
         return $this;
     }
 
@@ -102,12 +290,11 @@ class DB
         $where = '';
         //2.生成?对于值的数组
         $values = [];
+        //关系表达式
         $sep = '';
         foreach ($this->where as $w) {
             list($field, $op, $val) = $w;
-            $where .= "{$sep}`{$field}`{$op}? ";
-            //默认条件是AND
-            $sep = ' and ';
+            $where .= " {$sep} `{$field}` {$op} ? ";
             $values[] = $val;
         }
         $where = $where ? ' where ' . $where : $where;
@@ -121,15 +308,26 @@ class DB
      */
     public function get()
     {
-        $genwhere = $this->genWhere();
-        $limit = '';
-        if ($this->limit) {
-            $limit = " limit {$this->limit[0]},{$this->limit[1]} ";
+        $this->genSql();
+        $pdosmt = $this->pdo->prepare($this->sql['sql']);
+        $r = $pdosmt->execute($this->sql['value']);
+        if (!$r) {
+            print_r($pdosmt->errorInfo());
         }
-        //1.准备SQL
-        $sql = "select * from {$this->table} $genwhere[0] {$limit}";
-        $pdosmt = $this->pdo->prepare($sql);
-        $pdosmt->execute($genwhere[1]);
+        $this->clear();
         return $pdosmt->fetchAll();
+    }
+
+    /**
+     * 清空所有所需的条件
+     */
+    public function clear()
+    {
+        $this->where = [];
+        $this->table = '';
+        $this->limit = [];
+        $this->fields = '*';
+        $this->sql = [];
+        $this->join = '';
     }
 }
--- app/Controller/ArticleController.php
+++ app/Controller/ArticleController.php
@@ -9,11 +9,37 @@ class ArticleController extends BaseController
     public function index()
     {
         $db = new DB;
-        $data = $db->table('article')
+//        $data = $db->table('article')
+//            ->select('id', 'title')
 //            ->where('cate_id', '=', '10')
 //            ->where('user_id', '=', '6')
-//            ->getAll();
-            ->first();
-        var_dump($data);
+//            ->get();
+////            ->first();
+//        var_dump($data);
+
+//        $db->sql = [
+//            'sql' => 'select * from article where `title` like ?',
+//            'value' => ['文章1%']
+//        ];
+        $db->sql = [
+            'sql' => 'select article.id as aid,article.title,users.id as userid,users.username as 编辑人
+                      from article join users on article.user_id=users.id limit 2 ',
+            'value' => []
+        ];
+        $data = $db->get();
+        print_r($data);
+        echo '<br>';
+
+
+//        $data = $db->table('article')
+//            ->select('id', 'title')
+//            ->where('title', 'like', '文章1%')
+//            ->get();
+        $data = $db->table('article')
+            ->join('users', 'article.user_id=users.id')
+            ->select('article.id as aid', 'article.title', 'users.id as uid', 'users.username as 编辑人')
+            ->take(3)
+            ->get();
+        print_r($data);
     }
 }
